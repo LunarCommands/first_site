0ec282d124be6d78528fd831f33f1c9d519fd34e
Phil Elementz <tfeabp@gmail.com>
2022-01-26 13:54:30 +0100

    Added some nice animations

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let i = 0
-let txt = "I'm Lunar Commands"
-let speed = 80
+const txt = "I'm Lunar Commands"
+const speed = 80
+
+const faders = document.querySelectorAll('.fade-in')
 
 function typeWriter() {
   if (i < txt.length) {
@@ -10,3 +12,36 @@ function typeWriter() {
   }
 }
 typeWriter()
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("heading_animation").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter()
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+}
+
+const appearOnScroll = new IntersectionObserver (function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return
+    } else {
+      entry.target.classList.add('appear')
+      appearOnScroll.unobserve(entry.target)
+    }
+  })
+},
+appearOptions)
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader)
+})
